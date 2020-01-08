2124bf9a9dc15fb5a23b3766b3328f772fe8dd79
Mikołaj Wójcik <miknios@gmail.com>
2020-01-08 20:44:54 +0100

    repository initialized

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SceneController : MonoBehaviour, IPointerDownHandler
+{
+    public Snowman snowman;
+    public int lives;
+    public Button button;
+
+    private Snowman currentSnowman;
+
+    private void Awake()
+    {
+        button.onClick.AddListener(HandleSnowmanDied);
+        Lean.Touch.LeanTouch.OnFingerSwipe += HandleFingerSwipe;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (currentSnowman != null)
+                currentSnowman.Died -= HandleSnowmanDied;
+            currentSnowman = Instantiate(snowman);
+            currentSnowman.Died += HandleSnowmanDied;
+            currentSnowman.transform.localPosition = new Vector3(Random.RandomRange(-1, 1), Random.RandomRange(-1, 1), Random.RandomRange(-1, 1));
+        }
+    }
+
+    public void HandleSnowmanDied()
+    {
+        Debug.Log("Snowman died");
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        HandleSnowmanDied();
+    }
+
+    void HandleFingerSwipe(Lean.Touch.LeanFinger finger)
+    {
+        HandleSnowmanDied();
+    }
+}
---
+++ Assets/Scripts/Snowman.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Snowman : MonoBehaviour
+{
+    public event Action Died;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (Died != null)
+                Died();
+        }
+    }
+}
